1d759d3070da9c5232a0995fb2a5b5bd07f7cc5b
Santiago2998 <165844689+Santiago2998@users.noreply.github.com>
2026-02-27 21:25:46 -0600

    Add files via upload

---
+++ Clase 27 FEB/27FEBej0.c
@@ -0,0 +1,28 @@
+/*
+<stdlib.h>  => Permite usar funciones de memoria dinámica
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *arreglo, num, cont;
+
+    printf("Cuantos elementos tiene el arreglo?\n");
+    scanf("%d", &num);
+    arreglo = (int *)malloc (num * sizeof(int));
+    //                 Este espacio es el tamaño dinámico
+    //  La variable arreglo se encuentra en el stack y tiene un enlace con Heap para que esto vaya creciendo
+
+    if (arreglo != NULL){
+        printf("Arreglo reservado dinámicamente: \n\t[");
+        for (cont = 0; cont < num; cont++){
+            printf("\t%d", *(arreglo + cont));
+        }
+        printf("\t]\n");
+        printf("Se libera el espacio reservado. \n");
+        free(arreglo);
+    }
+
+    return 0;
+}
